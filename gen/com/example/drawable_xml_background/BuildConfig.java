0fc0d8e0161aefc94040f22f04717f04dd15744b
niulei@baihe <niulei@baihe.com>
2014-08-21 17:31:54 +0800

    first version

---
+++ gen/com/example/drawable_xml_background/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.drawable_xml_background;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
